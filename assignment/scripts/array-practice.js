c99247593e706c87692059a6d1870512fb9dd5fb
Dylan Howard <dylanhoward0424@outlook.com>
2021-06-27 01:40:25 -0500

    added favorite food to beginning

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -66,7 +66,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favoriteFoods.unshift('chickenPotPie');
+console.log(`Added a favorite food to beginning: ${favoriteFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
